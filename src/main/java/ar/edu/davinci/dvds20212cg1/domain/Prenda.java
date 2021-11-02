4f902d2449bdc5758b93a3639c8ff398ea653f84
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-11-01 22:24:23 -0300

    agrego conexion a bbdd + Clases Prenda, TipoPrenda

---
+++ src/main/java/ar/edu/davinci/dvds20212cg1/domain/Prenda.java
@@ -0,0 +1,49 @@
+package ar.edu.davinci.dvds20212cg1.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Config inicial JPA
+@Entity
+@Table(name = "prendas")
+
+// Config Lombok
+@NoArgsConstructor // Agrega constructor
+@AllArgsConstructor // Agrega constructor con todos los params
+@Data // Agrega todos los setter & getter
+@Builder // Crea una inner Class PrendaBuilder con un method build()
+public class Prenda {
+	// Config JPA de la PJ de la tabla
+	@Id
+	// Config JPA de la manera en que se generan los IDs autogenerados
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	// Config JPA del nombre de la columna
+	@Column(name = "prd_id")
+	private Long id;
+
+	@Column(name = "prd_descripcion")
+	private String descripcion;
+
+	@Column(name = "prd_tipo_prenda")
+	@Enumerated(EnumType.STRING)
+	private TipoPrenda tipo;
+
+	@Column(name = "prd_precio_base")
+	private BigDecimal precioBase;
+}
---
+++ src/main/java/ar/edu/davinci/dvds20212cg1/domain/TipoPrenda.java
@@ -0,0 +1,22 @@
+package ar.edu.davinci.dvds20212cg1.domain;
+
+public enum TipoPrenda {
+	SACO("Saco"),
+	PANTALON("Pantalon"),
+	CAMISA("Camisa"),
+	CAMPERA("Campera"),
+	TAPADO("Tapado"),
+	CHAQUETA("Chaqueta"),
+	MEDIA("Media"),
+	BUFANDA("Bufanda");
+	
+	private String descripcion;
+	
+	private TipoPrenda(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	private String getDescripcion() {
+		return descripcion;
+	}
+}
