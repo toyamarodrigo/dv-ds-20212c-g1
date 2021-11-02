7d94659d845580da13abcd8770fe8b9832fe5880
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-11-01 21:37:35 -0300

    Initial commit

---
+++ src/main/java/ar/edu/davinci/dvds20212cg1/DvDs20212cG1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20212cg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20212cG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20212cG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20212cg1/DvDs20212cG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20212cg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20212cG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
