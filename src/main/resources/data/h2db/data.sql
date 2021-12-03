66e6ecea2e64c12942d31a82b4076d7b9de272bd
rt <41844101+toyamarodrigo@users.noreply.github.com>
2021-12-03 01:30:51 -0300

    Update data.sql

--- src/main/resources/data/h2db/data.sql
+++ src/main/resources/data/h2db/data.sql
@@ -1,10 +1,17 @@
-INSERT INTO prendas
-(prd_id,
-prd_descripcion,
-prd_precio_base,
-prd_tipo_prenda) 
-VALUES 
-(1, 'Camisa Celeste',10.24,'CAMISA'),
-(2, 'Camisa Blanca',100.50,'CAMISA'),
-(3, 'Saco Vestir',102.40,'SACO'),
-(4, 'Pantalón Gabardina Beige',1004.00,'PANTALON'),
+insert
+	into
+	prendas (prd_id,
+	prd_descripcion,
+	prd_precio_base,
+	prd_tipo_prenda)
+values (1,'Camisa Celeste',10.24,'CAMISA'),
+(2,'Camisa Blanca',100.50,'CAMISA'),
+(3,'Saco Vestir',102.40,'SACO'),
+(4,'Pantalón Gabardina Beige',1004.00,'PANTALON');
+
+insert
+	into clientes (cli_id, cli_nombre, cli_apellido)
+values (1, 'Rodri','Toyama'),
+(2, 'Marcelo','Lujan'),
+(3, 'Malena','Tango'),
+(4, 'Federico','Rere');
