b914b0371838a74ed48b78ed782303b911a18ac1
Nat <nataliaoliveto94@gmail.com>
2021-12-19 17:25:14 -0300

    Update DDBB scripts

--- src/main/resources/data/h2db/data.sql
+++ src/main/resources/data/h2db/data.sql
@@ -1,18 +1,18 @@
-insert
-	into
+INSERT
+	INTO
 	prendas (prd_id,
 	prd_descripcion,
 	prd_precio_base,
 	prd_tipo_prenda)
-values (1,'Camisa Celeste',10.24,'CAMISA'),
+VALUES (1,'Camisa Celeste',10.24,'CAMISA'),
 (2,'Camisa Blanca',100.50,'CAMISA'),
 (3,'Saco Vestir',102.40,'SACO'),
-(4,'Pantalón Gabardina Beige',1004.00,'PANTALON');
+(4,'PantalÃ³n Gabardina Beige',1004.00,'PANTALON');
 
-insert
-	into clientes (cli_id, cli_nombre, cli_apellido)
-values (1, 'Rodri','Toyama'),
+INSERT
+	INTO clientes (cli_id, cli_nombre, cli_apellido)
+VALUES (1, 'Rodri','Toyama'),
 (2, 'Marcelo','Lujan'),
 (3, 'Malena','Tango'),
-(4, 'Federico','Rere')
+(4, 'Federico','Rere'),
 (5, 'Nat', 'Oli');
--- src/main/resources/data/h2db/schema.sql
+++ src/main/resources/data/h2db/schema.sql
@@ -13,33 +13,33 @@ DROP TABLE IF EXISTS prendas;
 DROP TABLE IF EXISTS clientes;
 
 create table negocios (
-	ngc_id bigint NOT NULL auto_increment,
-	ngc_sucursal varchar(255) DEFAULT null,
-	ngc_ganancia bigint default null,
-	primary key (ngc_id)
+	ngc_id BIGINT NOT NULL AUTO_INCREMENT,
+	ngc_sucursal VARCHAR(255) DEFAULT NULL,
+	ngc_ganancia BIGINT DEFAULT NULL,
+	PRIMARY KEY (ngc_id)
 );
 
 create TABLE clientes (
-  cli_id bigint NOT NULL AUTO_INCREMENT,
-  cli_apellido varchar(255) DEFAULT NULL,
-  cli_nombre varchar(255) DEFAULT NULL,
+  cli_id BIGINT NOT NULL AUTO_INCREMENT,
+  cli_apellido VARCHAR(255) DEFAULT NULL,
+  cli_nombre VARCHAR(255) DEFAULT NULL,
   PRIMARY KEY (cli_id)
 );
 
 create TABLE prendas (
-  prd_id bigint NOT NULL AUTO_INCREMENT,
-  prd_descripcion varchar(255) DEFAULT NULL,
-  prd_precio_base decimal(19,2) acDEFAULT NULL,
-  prd_tipo_prenda varchar(255) DEFAULT NULL,
+  prd_id BIGINT NOT NULL AUTO_INCREMENT,
+  prd_descripcion VARCHAR(255) DEFAULT NULL,
+  prd_precio_base DECIMAL(19,2) DEFAULT NULL,
+  prd_tipo_prenda VARCHAR(255) DEFAULT NULL,
   PRIMARY KEY (prd_id)
 );
 
 create TABLE ventas (
-  vta_id bigint NOT NULL AUTO_INCREMENT,
-  tipo_venta varchar(31) NOT NULL,
-  vta_fecha datetime(6) DEFAULT NULL,
-  vta_cli_id bigint DEFAULT NULL,
-  vta_ngc_id bigint default null,
+  vta_id BIGINT NOT NULL AUTO_INCREMENT,
+  tipo_venta VARCHAR(31) NOT NULL,
+  vta_fecha DATETIME(6) DEFAULT NULL,
+  vta_cli_id BIGINT DEFAULT NULL,
+  vta_ngc_id BIGINT DEFAULT NULL,
   PRIMARY KEY (vta_id),
   KEY vta_cli_fk (vta_cli_id),
   CONSTRAINT vta_cli_fk FOREIGN KEY (vta_cli_id) REFERENCES clientes (cli_id),
@@ -48,10 +48,10 @@ create TABLE ventas (
 );
 
 create TABLE venta_items (
-  itm_id bigint NOT NULL AUTO_INCREMENT,
-  itm_cantidad int DEFAULT NULL,
-  itm_prd_id bigint DEFAULT NULL,
-  itm_vta_id bigint NOT NULL,
+  itm_id BIGINT NOT NULL AUTO_INCREMENT,
+  itm_cantidad INT DEFAULT NULL,
+  itm_prd_id BIGINT DEFAULT NULL,
+  itm_vta_id BIGINT NOT NULL,
   PRIMARY KEY (itm_id),
   KEY itm_vta_fk (itm_vta_id),
   CONSTRAINT itm_vta_fk FOREIGN KEY (itm_vta_id) REFERENCES ventas (vta_id),
@@ -60,15 +60,15 @@ create TABLE venta_items (
 );
 
 create TABLE ventas_efectivo (
-  vta_id bigint NOT NULL,
+  vta_id BIGINT NOT NULL,
   PRIMARY KEY (vta_id),
   CONSTRAINT vte_vta_fk FOREIGN KEY (vta_id) REFERENCES ventas (vta_id)
 );
 
 create TABLE ventas_tarjeta (
-  vta_id bigint NOT NULL,
-  vtt_cantidad_cuotas int DEFAULT NULL,
-  vtt_coeficiente  decimal(2,2) DEFAULT NULL,
+  vta_id BIGINT NOT NULL,
+  vtt_cantidad_cuotas INT DEFAULT NULL,
+  vtt_coeficiente  DECIMAL(2,2) DEFAULT NULL,
   PRIMARY KEY (vta_id),
   CONSTRAINT vtt_vta_fk FOREIGN KEY (vta_id) REFERENCES ventas (vta_id)
 );
